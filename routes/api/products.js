b2154d1f81e81708618e704dd96c23488c2c6611
Lilysaria <bloomyday56@gmail.com>
2024-07-11 16:04:28 -0700

    refactor(api): remove unused API routes and controllers
    - Remove createProduct and updateProduct routes and controller functions
    - Apply linter suggestions for improved code quality

--- routes/api/products.js
+++ routes/api/products.js
@@ -2,15 +2,8 @@ const express = require('express');
 const router = express.Router();
 const products = require('../../controllers/products');
 
-
-// route for creating a new product
-router.post('/', products.createProduct);
-
-// route for listing all products
 router.get('/', products.listProducts);
-router.post('/', products.createProduct);
-router.get('/:productId', products.viewProduct);
 
-router.put('/:productId', products.updateProduct);
+router.get('/:productId', products.viewProduct);
 
 module.exports = router;
