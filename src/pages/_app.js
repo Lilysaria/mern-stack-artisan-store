87172362fcdd43d07d6cdeea54a870c52bcdb1ed
Lilysaria <bloomyday56@gmail.com>
2024-10-12 18:36:02 -0700

    refactor(auth): merge App.jsx functionality into _app.js
    - Remove App.jsx file
    - Update _app.js to include user authentication and routing logic

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,33 @@
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import NavBar from '../components/NavBar/NavBar';
 import userService from '../utils/userService';
 import '../../globals.css';
 
 function MyApp({ Component, pageProps }) {
   const [user, setUser] = useState(null);
+  const router = useRouter();
 
   useEffect(() => {
     const loggedInUser = userService.getUser();
     if (loggedInUser) setUser(loggedInUser);
   }, []);
 
+  function handleSignUpOrLogin() {
+    const loggedInUser = userService.getUser();
+    if (loggedInUser) setUser(loggedInUser);
+  }
+
   function handleLogout() {
     userService.logout();
     setUser(null);
+    router.push('/');
   }
 
   return (
     <div className="app-container">
       <NavBar user={user} onLogout={handleLogout} />
-      <Component {...pageProps} />
+      <Component {...pageProps} user={user} handleSignUpOrLogin={handleSignUpOrLogin} />
     </div>
   );
 }
