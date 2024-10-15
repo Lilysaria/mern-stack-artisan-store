7062c595fc674224b32a9391d8d1a841d434c9c2
Lilysaria <bloomyday56@gmail.com>
2024-10-15 10:43:41 -0700

    chore(config): attempt to resolve routing and static file serving issues

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,18 @@
-export default function Home() {
-    return (
-      <div>
-        <h1>boiler homepage</h1>
-      </div>
-    );
-  }
-  
+import React from 'react';
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Artisan Store</h1>
+      <nav>
+        <Link href="/products">Products</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/cart">Cart</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Home;
