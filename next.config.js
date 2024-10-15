a287b847490f836aedf662456fabd75781c35745
Lilysaria <bloomyday56@gmail.com>
2024-10-15 10:10:53 -0700

    chore(config): attempt to resolve 404 errors in vercel website

--- next.config.js
+++ next.config.js
@@ -5,19 +5,6 @@ const nextConfig = {
   },
   output: 'standalone',
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Set-Cookie',
-            value: '__vercel_live_token=; Path=/; SameSite=None; Secure',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
